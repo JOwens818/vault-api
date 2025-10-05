8690490733d824a5b5c1e299438f2035928c4229
Jim Owens <JOwens818@gmail.com>
2025-10-05 09:40:13 -0400

    comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-//import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
